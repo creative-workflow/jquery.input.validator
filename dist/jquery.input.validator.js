f223f27e33d1369783ab105f008737fad1c911ed
Tom Hanoldt <tom@creative-workflow.berlin>
2018-03-31 07:53:32 +0200

    dist v1.0.8

--- dist/jquery.input.validator.js
+++ dist/jquery.input.validator.js
@@ -158,7 +158,7 @@
       this.init = bind(this.init, this);
       this.config = this.constructor.config;
       this.ns = 'ivalidator';
-      this.version = '1.0.7';
+      this.version = '1.0.8';
       this.init(config);
     }
 
